5134c80a4c270f976732bc8f8b08ebb6f2488553
Elromario15 <romatheory@gmail.com>
2022-04-13 20:13:39 -0500

    ejercicios de condicionales

---
+++ primera prueba.py
@@ -0,0 +1,14 @@
+#print("hola soy romario")
+
+nombre= "Romario"
+
+numero= 20
+
+#print(nombre)
+#print(numero)
+
+persona_masculina = True
+
+nombreingresado= str(input("ingresa tu nombre: "))
+
+print(f"hola joven: {nombreingresado}, su edad es de: {numero +8}, su su segundo nombre es: {nombre}")
